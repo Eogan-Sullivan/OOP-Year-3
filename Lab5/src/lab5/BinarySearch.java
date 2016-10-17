c0d0d54c167aa69a75646cfa4bca3e131d2911ff
Eogan Sullivan <eogan.sullivan@outlook.com>
2016-10-17 12:34:56 +0100

    binary search v2
    binary search v2

--- Lab5/src/lab5/BinarySearch.java
+++ Lab5/src/lab5/BinarySearch.java
@@ -11,5 +11,36 @@ package lab5;
  * @author t00166087
  */
 public class BinarySearch {
+     
     
+    public static int binSearch(int []array,int key)
+    {
+        return binSearchSub(array,key,0,array.length-1);
+    }
+    
+    
+    private static int binSearchSub(int []array,int key,int low,int high)
+    {
+        int middle = (low+high)/2;
+        
+        if(high < low)
+        {
+            return -1;
+        }
+       
+               if ( key == array[ middle ] )  // match
+                    return middle;
+               else if ( key > array[ middle ] )
+                  return binSearchSub(array,key,middle+1,high);
+               else 
+                   return binSearchSub(array,key,low,middle-1);
+      
+  
+              
+    }
 }
+    
+   
+   
+
+
